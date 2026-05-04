3208aef6280494a6004b8f30295ef34682463110
Edoardo Lenzi <31968677+EdoardoLenzi9@users.noreply.github.com>
2026-05-04 21:30:05 +0000

    main

--- 007.py
+++ 007.py
@@ -12,3 +12,11 @@
 # se la variabile x >= 18 stampa "sei maggiorenne"
 # se la variabile x <  18 stampa "sei minorenne"
 # se la variabile x >  50 stampa "sei over cinquanta"
+
+x: int = int(input("insersci eta: "))
+if x < 18:
+    print("sei minorenne")
+elif x >= 18 and x < 50:
+    print("sei maggiorenne")
+else:
+    print("sei over cinquanta")
